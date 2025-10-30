4d47e857ef0a20cdfc2a0936affbb121db0d46a3
Michael Turco <michael.turco2@snhu.edu>
2025-10-30 16:46:54 -0400

    remove 'zybooks' package name

--- app/src/main/java/com/zybooks/turco_michael_weight_tracking/CustomNotifications.java
+++
@@ -1,8 +0,0 @@
-package com.zybooks.turco_michael_weight_tracking;
-
-public class CustomNotifications{
-    // TODO: make class to handle sending notifications
-
-    // all the work is done up to the point of sending the notification,
-    // just need a single function to create a notification with text input
-}
