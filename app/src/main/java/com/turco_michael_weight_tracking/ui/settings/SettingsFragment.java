d9b77b830c0361e812326f32339342ef054dc85b
Michael Turco <michael.turco2@snhu.edu>
2025-11-11 15:49:58 -0500

    lots of progress on measurement unit selector area in the settings menu. toggle buttons with custom colors, saving and loading properly too!

--- app/src/main/java/com/turco_michael_weight_tracking/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/turco_michael_weight_tracking/ui/settings/SettingsFragment.java
@@ -10,10 +10,12 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.turco_michael_weight_tracking.LocalStorage;
+import com.turco_michael_weight_tracking.LocalStorage.MeasurementUnit;
 import com.turco_michael_weight_tracking.R;
 import com.turco_michael_weight_tracking.databinding.FragmentSettingsBinding;
 
@@ -32,6 +34,7 @@ public class SettingsFragment extends Fragment {
         setupTextWatcher();
         setupButtonEvents();
         updateNotificationStatusText();
+        updateMeasurementUnitSelected();
 
         return root;
     }
@@ -60,9 +63,15 @@ public class SettingsFragment extends Fragment {
     }
 
     private void setupButtonEvents() {
+        // Goal weight 'save & apply' button
         binding.editGoalWeightApply.setOnClickListener(v -> clickGoalWeightApplyButton());
 
+        // Toggle notifications button
         binding.toggleNotifications.setOnClickListener(v -> clickToggleNotificationsButton());
+
+        // Measurement unit buttons
+        binding.measurementPounds.setOnClickListener(v -> clickMeasurementButton(MeasurementUnit.POUNDS));
+        binding.measurementKilograms.setOnClickListener(v -> clickMeasurementButton(MeasurementUnit.KILOGRAMS));
     }
 
     private void clickGoalWeightApplyButton() {
@@ -89,6 +98,32 @@ public class SettingsFragment extends Fragment {
             // clicking should try to enable notifications
             displayNotificationRequest();
         }
+    }
+
+    private void clickMeasurementButton(MeasurementUnit unit) {
+        storage.setMeasurementUnit(unit);
+        updateMeasurementUnitSelected();
+    }
+
+    private void updateMeasurementUnitSelected() {
+        MeasurementUnit unit = storage.getMeasurementUnit();
+
+        int notSelected = ContextCompat.getColor(requireContext(), R.color.gray_medium);
+        int selected = ContextCompat.getColor(requireContext(), R.color.lime_medium);
+
+        binding.measurementPounds.setBackgroundColor(notSelected);
+        binding.measurementKilograms.setBackgroundColor(notSelected);
+
+        switch (unit) {
+            case POUNDS:
+                binding.measurementPounds.setBackgroundColor(selected);
+                break;
+            case KILOGRAMS:
+                binding.measurementKilograms.setBackgroundColor(selected);
+                break;
+        }
+
+        // got help from https://stackoverflow.com/questions/13842447/android-set-button-background-programmatically
     }
 
     private boolean hasValidWeightText(EditText text) {
