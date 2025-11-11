ed912217bff36fb9de868234685c67962fb15bdc
Michael Turco <michael.turco2@snhu.edu>
2025-11-11 16:39:49 -0500

    add more helper functions to unit converter

--- app/src/main/java/com/turco_michael_weight_tracking/UnitConverter.java
+++ app/src/main/java/com/turco_michael_weight_tracking/UnitConverter.java
@@ -1,16 +1,23 @@
 package com.turco_michael_weight_tracking;
 
+import android.content.Context;
+
 import com.turco_michael_weight_tracking.LocalStorage.MeasurementUnit;
+
+import java.util.Locale;
 
 /*
     Helper class for converting between units of weight
+
+    note:
+    all weights stored in local storage are in pounds
 
     got conversion formulas from
     https://www.rapidtables.com/convert/weight/kg-to-pound.html
  */
 
 public class UnitConverter {
-    public static float convertUnitToPounds(float value, MeasurementUnit unit) {
+    public static float unitToPounds(float value, MeasurementUnit unit) {
         switch (unit) {
             case POUNDS:
                 return value;
@@ -21,7 +28,7 @@ public class UnitConverter {
         }
     }
 
-    public static float convertUnitToKilograms(float value, MeasurementUnit unit) {
+    public static float unitToKilograms(float value, MeasurementUnit unit) {
         switch (unit) {
             case POUNDS:
                 return value / 0.45359237f;
@@ -32,16 +39,47 @@ public class UnitConverter {
         }
     }
 
-    public static float convertUnitToUnit(float value, MeasurementUnit unit, MeasurementUnit toUnit) {
+    public static float unitToUnit(float value, MeasurementUnit unit, MeasurementUnit toUnit) {
         if (unit == toUnit) return value;
 
         switch (toUnit) {
             case POUNDS:
-                return convertUnitToPounds(value, unit);
+                return unitToPounds(value, unit);
             case KILOGRAMS:
-                return convertUnitToKilograms(value, unit);
+                return unitToKilograms(value, unit);
             default:
                 return -1;
+        }
+    }
+
+    // returns a formatted string like '123.4 lbs'
+    public static String poundsToFormattedUnitString(Context context, float value, MeasurementUnit unit) {
+        String unitString = getShortUnitString(context, unit);
+        value = unitToUnit(value, MeasurementUnit.POUNDS, unit);
+        return String.format(Locale.getDefault(), "%.1f %s", value, unitString);
+    }
+
+    // returns a string like "lbs"
+    public static String getShortUnitString(Context context, MeasurementUnit unit) {
+        switch (unit) {
+            case POUNDS:
+                return context.getString(R.string.measurement_pounds_short);
+            case KILOGRAMS:
+                return context.getString(R.string.measurement_kilograms_short);
+            default:
+                return "";
+        }
+    }
+
+    // returns a string like "pounds (lbs)"
+    public static String getLongUnitString(Context context, MeasurementUnit unit) {
+        switch (unit) {
+            case POUNDS:
+                return context.getString(R.string.measurement_pounds);
+            case KILOGRAMS:
+                return context.getString(R.string.measurement_kilograms);
+            default:
+                return "";
         }
     }
 }
